dc92a8e1260181d9d811a50a39f22a40d6cb7243
Antsa Randriamihaja <arandriamihaja@gmail.com>
2017-05-11 15:53:13 -0400

    app configuration set up

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
+const config = require('./config');
+const stripe = require('stripe')(config.secret_key);
+const bodyParser = require('body-parser');
+
 var port = process.env.PORT || 3000;
 var app = express();
+
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res)=>{
     res.render(path.join(__dirname, './views/index.hbs'))
 })
 
 app.post('/getCardToken', (req, res)=>{
-    res.send('Got token')
+    var token = req.body.stripeToken;
+  
+    res.send(token)
 })
 
 
